d60ae1c662750cd79fc0b7a902ef81295413ae50
NG YONG SHENG <gnehsheng@gmail.com>
2022-05-14 16:47:00 +0800

    .

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken')
 const users = require('./users')
 const transactions = require('./transactions')
 const PORT = process.env.PORT ?? 5000
+const path = require('path')
 
 app.use(express.json())
 app.use(express.static('./jwt-frontend/build'))
@@ -28,13 +29,14 @@ const verifyToken = (req, res, next) => {
     }
 }
 
-app.get('/', (req, res) => {
+
+app.get('/api', (req, res) => {
     res.send('hello world')
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/login', (req, res) => {
     const { username, password } = req.body
-
+    
     if (users[username].password === password) {
         //authenticate and create the jwt
         const newToken = jwt.sign(
@@ -43,8 +45,8 @@ app.post('/login', (req, res) => {
             },
             process.env.TOKEN_SECRET,
             { expiresIn: 60 * 60 }
-        )
-
+            )
+            
         res
         .status(200)
         .cookie('NewCookie', newToken, { path: '/', httpOnly: true })
@@ -55,18 +57,21 @@ app.post('/login', (req, res) => {
 })
 //testing
 
-app.post('/post', verifyToken, (req, res) => {
-
+app.post('/api/post', verifyToken, (req, res) => {
+    
     const username = req.user
     const userTransactions = transactions[username]
-
+    
     res.status(200).json({ transactions: userTransactions })
 })
 
-app.post('/logout', (req, res)=>{
+app.post('/api/logout', (req, res)=>{
     res.clearCookie('NewCookie').send('cookie dead')
 })
 
+app.get('/*', (req, res)=>{
+    res.sendFile(path.join(__dirname, './jwt-frontend/build/index.html'))
+})
 
 app.listen(PORT, () => {
     console.log('Listening at port 3000')
