571e06771e0847219a993572649d0414a20ea154
NG YONG SHENG <gnehsheng@gmail.com>
2022-05-14 16:42:42 +0800

    fixing static

--- jwt-frontend/src/App.js
+++ jwt-frontend/src/App.js
@@ -1,11 +1,43 @@
-import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
+
+const Home = () => {
+  return "Home"
+}
+
+const Post = () => {
+  return "Posts"
+}
+
+const Login = () => {
+  const handleLogin = () => {
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username: 'gnehsheng', password: '123' })
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+  }
+  return <button onClick={handleLogin}>Login</button>
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>HI</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
