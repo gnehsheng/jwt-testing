9b32d951ee78511269a881add4d1259e7dcdf51a
NG YONG SHENG <gnehsheng@gmail.com>
2022-05-14 12:08:10 +0800

    testing

---
+++ transactions.js
@@ -0,0 +1,12 @@
+const transactions = {
+    gnehsheng:{
+        day1: '$2',
+        day2: '$3',
+    },
+    gnehsheng2:{
+        day1: '$100',
+        day2: '$150'
+    },
+}
+
+module.exports = transactions
---
+++ users.js
@@ -0,0 +1,10 @@
+const users = {
+    'gnehsheng': {
+        password: '123',
+    },
+    'gnehsheng2': {
+        password: '12345'
+    }
+}
+
+module.exports = users
